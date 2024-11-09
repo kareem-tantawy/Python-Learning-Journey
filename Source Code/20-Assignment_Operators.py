53c0335404adbf3601fb61ca8766a39d0371b6b4
Karim Tantawy <kareemtantawy01@gmail.com>
2024-11-09 19:56:51 +0200

    Operators lessons

---
+++ Source Code/20-Assignment_Operators.py
@@ -0,0 +1,34 @@
+# -------------------------- #
+# -- Assignment Operators -- #
+# -------------------------- #
+
+# --------------------------
+# =
+# +=
+# -=
+# *=
+# /=
+# **=
+# %=
+# //=
+# --------------------------
+
+x = 10  # Var One
+y = 20  # Var Two
+
+# Var One = Self [Operator] Var Two
+# Var One [Operator]= Var Two
+
+x += y
+print(x)
+
+x -= y
+print(x)
+
+z = 5
+z *= 2
+print(z)
+
+u = 10
+u /= 2
+print(u)
