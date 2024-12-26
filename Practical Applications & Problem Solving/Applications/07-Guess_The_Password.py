f097b9b5c5f1ca078f553451421977c23317a744
Karim Tantawy <kareemtantawy01@gmail.com>
2024-12-26 08:22:56 +0200

    Fixed some issues and added new features in case of right or wrong

--- Practical Applications & Problem Solving/Applications/07-Guess_The_Password.py
+++ Practical Applications & Problem Solving/Applications/07-Guess_The_Password.py
@@ -2,6 +2,8 @@
 # You will have to guess a number from 1 to 10
 
 import random
+import pyfiglet
+import termcolor
 
 
 flag = True
@@ -9,18 +11,30 @@ flag = True
 while flag:
     secret = random.randint(1, 10)
     for i in range(3):
-        if i == 3:
-            break
         guess = int(
             input("I'm thinking of a number between 1 and 10. Can you guess it? ")
         )
         if guess == secret:
-            print("Congratulations, you guessed it!")
+            print(
+                termcolor.colored(
+                    pyfiglet.figlet_format("Congratulations, you guessed it!"),
+                    color="green",
+                )
+            )
             break
-        elif guess > secret:
+        if i == 2:
+            continue
+        if guess > secret:
             print("Nope, your guess is a bit high. Give it another shot!")
         else:
             print("Nope, your guess is a bit low. Give it another shot!")
+    else:
+        print(
+            termcolor.colored(
+                pyfiglet.figlet_format("Oops, you've finished your 3 tries"),
+                color="red",
+            )
+        )
     msg = input("Wanna play again? (yes/no) ")
     if msg == "y" or msg == "yes":
         flag = True
